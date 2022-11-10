302ed99740010d5c26fa7bf36687885a3f4a24dd
Jack Kolb <jackhunterkolb@gmail.com>
2022-11-10 10:37:04 -0500

    )

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
+import os
 
 if __name__ == "__main__":
   count = 1
   while True:
-    print(count)
+    print(count, "env:", os.environment.get("TEST_ENV"))
     count += 1
     time.sleep(1)
